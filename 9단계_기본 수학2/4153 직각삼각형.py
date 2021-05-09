055ff25e4a8a3197e5892ada5dc0fc71406a160c
Alswl_ya <71184811+RapunzeI@users.noreply.github.com>
2021-05-10 03:15:08 +0900

    Basic Mathematics

---
+++ 9단계_기본 수학2/4153 직각삼각형.py
@@ -0,0 +1,13 @@
+Triangle = []
+while True:
+    lst = list(map(int, input().split()))
+    if  lst[0] == 0 and lst[1] == 0 and lst[2] == 0:
+        break
+    lst.sort()
+    Triangle.append(lst)
+
+for lst in Triangle:
+    if lst[2]**2 == lst[0]**2 + lst[1]**2:
+        print('right')
+    else:
+        print('wrong')
